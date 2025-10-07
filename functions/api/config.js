38b1a33c39a5de79cf993db299a5e34644204ce3
雾都 <icp@scfc.top>
2025-10-07 12:10:11 +0800

    feat: 更新赞赏页面配置并替换资源文件
    更新配置文件中的默认值，包括背景图片URL、favicon、二维码路径和联系信息
    替换旧的.webp图片资源为新的.png和.jpg格式
    删除不再使用的.webp图片文件和README.md文档

--- functions/api/config.js
+++ functions/api/config.js
@@ -2,21 +2,21 @@ export async function onRequest(context) {
   const { env } = context;
 
   const config = {
-    backgroundImageUrl: env.CONFIG_BACKGROUND_IMAGE_URL || "/img/bj.webp", 
+    backgroundImageUrl: env.CONFIG_BACKGROUND_IMAGE_URL || "https://bing.liushen.fun/api/daily/", 
     // --- MODIFIED --- Changed the default page title
     pageTitle: env.CONFIG_PAGE_TITLE || "赞赏页面",
-    favicon: env.CONFIG_FAVICON || "/img/icon.webp",
+    favicon: env.CONFIG_FAVICON || "/img/favicon.ico",
     // --- MODIFIED --- Changed the default banner title to match the screenshot
     bannerTitle: env.CONFIG_BANNER_TITLE || "您的赞赏，是我的续命咖啡~",
     qrCodes: {
-      wechat: env.CONFIG_QR_WECHAT || "/img/zsm_wx.webp",
-      alipay: env.CONFIG_QR_ALIPAY || "/img/zsm_zfb.webp",
+      wechat: env.CONFIG_QR_WECHAT || "/img/zsm_wx.png",
+      alipay: env.CONFIG_QR_ALIPAY || "/img/zsm_zfb.jpg",
     },
     infoBox: {
       text: env.CONFIG_INFO_TEXT || "如需修改昵称信息，您可以将您的赞赏截图和转账单号/支付单号和修改昵称发送到",
-      email: env.CONFIG_INFO_EMAIL || "zrf@zrf.me",
+      email: env.CONFIG_INFO_EMAIL || "qxbk@qq.com",
     },
-    footerHTML: env.CONFIG_FOOTER_HTML || `周润发: <a href="https://d.zrf.me/blog">博客</a>`,
+    footerHTML: env.CONFIG_FOOTER_HTML || `筱序二十: <a href="https://qxzhan.cn/">青序栈</a> | <a href="/admin">后台</a>`,
     pagination: {
       rowsPerPage: parseInt(env.CONFIG_ROWS_PER_PAGE, 10) || 10,
     },
